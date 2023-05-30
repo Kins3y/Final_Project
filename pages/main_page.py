42ef6ae4dd079309bb553e9a2d7991dcf4a877d6
Kins3y <nemstas95@gmail.com>
2023-05-31 00:50:18 +0200

    Added new dependencies

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,9 @@
 from selenium.common.exceptions import NoSuchElementException
 from .base_page import BasePage
-from selenium.webdriver.common.by import By
+from .locators import MainPageLocators
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
 
     def is_element_present(self, how, what):
         try:
@@ -13,4 +12,4 @@ class MainPage(BasePage):
             return False
         return True
     def should_be_login_link(self):
-        assert self.is_element_present(By.CSS_SELECTOR, "#login_link_invalid"), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
