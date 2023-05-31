64e744ca8594ec72578c41a986c4a750a703b58b
Kins3y <nemstas95@gmail.com>
2023-05-31 14:59:36 +0200

    deleted unused import

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,7 @@
 from selenium.common.exceptions import NoSuchElementException
 from .base_page import BasePage
 from .locators import MainPageLocators
+from .login_page import LoginPage
 class MainPage(BasePage):
     def go_to_login_page(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
