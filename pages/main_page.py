9b1cdd7dde689cf821695c7a877bb6b15dd2855e
Kins3y <nemstas95@gmail.com>
2023-06-02 18:19:17 +0200

    Hotfix

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,16 +1,11 @@
-from selenium.common.exceptions import NoSuchElementException
 from .base_page import BasePage
 from .locators import MainPageLocators
-from .login_page import LoginPage
+
 class MainPage(BasePage):
     def go_to_login_page(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        #return LoginPage(browser=self.browser, url=self.browser.current_url)
 
-    def is_element_present(self, how, what):
-        try:
-            self.browser.find_element(how, what)
-        except NoSuchElementException:
-            return False
-        return True
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
