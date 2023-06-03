5825eda0a36180d9f1726f18a3ac3fabe3fa8cda
Kins3y <nemstas95@gmail.com>
2023-06-03 12:58:40 +0200

    parametrize added

--- test_product_page.py
+++ test_product_page.py
@@ -2,10 +2,12 @@ from .pages.product_page import ProductPage
 import pytest
 import time
 
-link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+#link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=offer7'
 @pytest.mark.product_page
 class TestProductPage():
-    def test_guest_can_add_product_to_basket(self, browser):
+    @pytest.mark.parametrize('num_link',['0','1','2','3','4','5','6', pytest.param('7', marks=pytest.mark.xfail),'8','9'])
+    def test_guest_can_add_product_to_basket(self, browser, num_link):
+        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{num_link}"
         page = ProductPage(browser, link)
         page.open()
         page.should_be_correct_url()
@@ -13,3 +15,4 @@ class TestProductPage():
         page.solve_quiz_and_get_code()
         page.should_be_success_message()
         page.should_be_correct_item_added()
+        page.should_be_correct_basket_summary()
