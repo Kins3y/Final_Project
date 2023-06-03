1953c58a55a2703f171a6ab4b7ce65e891b17ade
Kins3y <nemstas95@gmail.com>
2023-06-03 16:25:21 +0200

    New locators for LoginPage and BasePage added New class BasketPageLocators added

--- pages/locators.py
+++ pages/locators.py
@@ -6,6 +6,10 @@ class MainPageLocators():
 class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+    REGISTER_EMAIL_ADDRESS = (By.CSS_SELECTOR, '#id_registration-email')
+    REGISTER_PASSWORD = (By.CSS_SELECTOR, '#id_registration-password1')
+    REGISTER_PASSWORD_CONFIRM = (By.CSS_SELECTOR, '#id_registration-password2')
+    REGISTER_BUTTON = (By.CSS_SELECTOR, '[name="registration_submit"]')
 
 class ProductPageLocators():
     BASKET_BUTTON = (By.CSS_SELECTOR, '#add_to_basket_form > button')
@@ -18,3 +22,10 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    SHOW_BASKET_BUTTON = (By.CSS_SELECTOR, 'span > a')
+    USER_ICON = (By.CSS_SELECTOR, '.icon-user')
+
+class BasketPageLocators():
+    BASKET_ITEMS = (By.CSS_SELECTOR, '[class="basket-items"]')
+    EMPTY_BASKET = (By.CSS_SELECTOR, '#content_inner > p')
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
