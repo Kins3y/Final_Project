079b7836026a97e83ff671976761a1f563a2b473
Kins3y <nemstas95@gmail.com>
2023-06-03 14:04:57 +0200

    is_not_element_present is_disappeared added, go_to_login_page and should_be_login_link rebased from main_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,5 +1,7 @@
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException, NoAlertPresentException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from .locators import BasePageLocators
 import math
 class BasePage():
     def __init__(self, browser, url, timeout=20):
@@ -10,11 +12,24 @@ class BasePage():
     def open(self):   # открываet нужную страницу в браузере, используя метод get().
         self.browser.get(self.url)
 
-
     def is_element_present(self, how, what): # поиск элемента на странице
         try:
             self.browser.find_element(how, what)
         except (NoSuchElementException):
+            return False
+        return True
+
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
             return False
         return True
 
@@ -31,3 +46,10 @@ class BasePage():
             alert.accept()
         except NoAlertPresentException:
             print("No second alert presented")
+
+    def go_to_login_page(self):
+        link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        link.click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
