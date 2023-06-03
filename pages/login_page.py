a3cb304c1b77a80610286a0bda95747f3886a30b
Kins3y <nemstas95@gmail.com>
2023-06-03 16:25:32 +0200

    register_new_user added

--- pages/login_page.py
+++ pages/login_page.py
@@ -18,3 +18,11 @@ class LoginPage(BasePage):
     def should_be_register_form(self):
         assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Registration form is not present"
         assert True
+
+    def register_new_user(self, email, password):
+        self.should_be_register_form()
+        self.input_text(*LoginPageLocators.REGISTER_EMAIL_ADDRESS, email)
+        self.input_text(*LoginPageLocators.REGISTER_PASSWORD, password)
+        self.input_text(*LoginPageLocators.REGISTER_PASSWORD_CONFIRM, password)
+        self.click_button(*LoginPageLocators.REGISTER_BUTTON)
+
